b42a99539ca4d82d9190271b90306f5c80d673b6
Shuwen Zheng <55172213+SUE7EN@users.noreply.github.com>
2019-11-04 13:49:33 -0800

    Create pots.ino

---
+++ pots.ino
@@ -0,0 +1,20 @@
+int led = 13;
+
+void setup() {
+  pinMode (led, OUTPUT);
+  Serial.begin(19200);
+  pinMode(A0, INPUT);
+  }
+
+void loop() {
+  int value;
+  value = analogRead(A0);
+  Serial.println (value);
+  if (value >= 50) {
+    digitalWrite(led, HIGH);
+    Serial.println("turning the LED on");
+  } else {
+    digitalWrite(led, LOW);
+    Serial.println("turning the LED off");
+  }
+}
